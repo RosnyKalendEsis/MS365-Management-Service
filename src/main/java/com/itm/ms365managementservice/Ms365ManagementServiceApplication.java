05f0ef4c475776dceda5b73f614b7f7dda1eea52
Rosny Kalend <rosnykalendkab@gmail.com>
2025-08-03 14:01:04 +0200

    First Commit

---
+++ src/main/java/com/itm/ms365managementservice/Ms365ManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itm.ms365managementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms365ManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms365ManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itm/ms365managementservice/Ms365ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itm.ms365managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms365ManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
